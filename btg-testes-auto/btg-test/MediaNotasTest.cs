9725d62e6eeb3b8b28cd15a3257a3d5a109ae983
Gabriela Zanotto Bosshard <gzbosshard@gmail.com>
2023-12-01 21:29:31 -0300

    implementação dos testes

---
+++ btg-testes-auto/btg-test/MediaNotasTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using btg_testes_auto;
+
+namespace btg_test
+{
+    public class MediaNotasTest
+    {
+        [Fact]
+        public void calculaMedia_MenorQueSete_Reprovado()
+        {
+            //Arrange
+            MediaNotas media = new();
+
+            List<decimal> listaNotas = new() { 0, 10, 2 };
+
+            //Act
+            string resultado = media.CalculaMedia(listaNotas, 5);
+
+            //Assert
+            Assert.Contains("não", resultado);
+        }
+
+        [Fact]
+        public void calculaMedia_MenorQueSete_Aprovado()
+        {
+            //Arrange
+            MediaNotas media = new();
+
+            List<decimal> listaNotas = new() { 8, 10, 2 };
+
+            //Act
+            string resultado = media.CalculaMedia(listaNotas, 8);
+
+            //Assert
+            Assert.Contains("Parabéns", resultado);
+        }
+
+        [Fact]
+        public void calculaMedia_MaiorQueSete_Aprovado()
+        {
+            //Arrange
+            MediaNotas media = new();
+
+            List<decimal> listaNotas = new() { 8, 10, 4 };
+
+            //Act
+            string resultado = media.CalculaMedia(listaNotas, 0);
+
+            //Assert
+            Assert.Contains("Parabéns", resultado);
+        }
+    }
+}
---
+++ btg-testes-auto/btg-test/MedialdadesTest.cs
@@ -0,0 +1,44 @@
+﻿using btg_testes_auto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace btg_test
+{
+    // nomeMetodo_CondicaoCenario_ComportamentoEsperado():
+    public class MedialdadesTest
+    {
+        [Fact]
+        public void calculaIdade_MaiorDezoito_Verdadeiro()
+        {
+            //Arrange
+            List<int> lista = new() {15, 20, 30};
+
+            MediaIdades mediaIdades = new();
+
+            //Act
+            decimal resultado = mediaIdades.CalculaMedia(lista);
+
+            //Assert
+            Assert.Equal(25, resultado);
+        }
+
+        [Fact]
+        public void calculaIdade_MaiorDezoito_Falso()
+        {
+            //Arrange
+            List<int> lista = new() { 15, 10, 16};
+
+            MediaIdades mediaIdades = new();
+
+            //Act
+            Action resultado = () => mediaIdades.CalculaMedia(lista);
+
+            //Assert
+            Assert.Throws<DivideByZeroException>(resultado);
+        }
+    }
+}
